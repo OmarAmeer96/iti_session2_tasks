67bf1b17b8398c759eefba9b3b8766ec21d212cb
Omar_Ameer <omar.ameer244@gmail.com>
2023-07-29 09:11:16 +0300

    Final Commit (Final Edit)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:iti_flutter_session2/screens/home_screen.dart';
-import 'package:page_transition/page_transition.dart';
-import 'package:lottie/lottie.dart';
+import 'package:iti_flutter_session2/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,48 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: Lottie.asset("assets/images/HouseAnimation.json"),
-
-      // Column(
-      //   children: [
-      //     Image.asset(
-      //       "assets/images/logo.png",
-      //       width: 200,
-      //       height: 200,
-      //     ),
-      //     const SizedBox(
-      //       height: 10,
-      //     ),
-      //     const Text(
-      //       "DreamVista Realty",
-      //       style: TextStyle(
-      //         fontFamily: "JosefinSans",
-      //         fontSize: 35,
-      //         fontWeight: FontWeight.bold,
-      //         color: Color(0xffFEFAE0),
-      //       ),
-      //     )
-      //   ],
-      // ),
-      backgroundColor: const Color(0xffDDA15E),
-      nextScreen: const HomeScreen(),
-      splashIconSize: 350,
-      duration: 2100,
-      splashTransition: SplashTransition.fadeTransition,
-      pageTransitionType: PageTransitionType.fade,
+      routes: {
+        HomeScreen.id: (context) => const HomeScreen(),
+        SplashScreen.id: (context) => const SplashScreen(),
+      },
+      initialRoute: SplashScreen.id,
     );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:iti_flutter_session2/screens/home_screen.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:lottie/lottie.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
@@ -6,13 +11,36 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("HI"),
-        ),
-      ),
+    return AnimatedSplashScreen(
+      splash: Lottie.asset("assets/images/HouseAnimation.json"),
+
+      // Column(
+      //   children: [
+      //     Image.asset(
+      //       "assets/images/logo.png",
+      //       width: 200,
+      //       height: 200,
+      //     ),
+      //     const SizedBox(
+      //       height: 10,
+      //     ),
+      //     const Text(
+      //       "DreamVista Realty",
+      //       style: TextStyle(
+      //         fontFamily: "JosefinSans",
+      //         fontSize: 35,
+      //         fontWeight: FontWeight.bold,
+      //         color: Color(0xffFEFAE0),
+      //       ),
+      //     )
+      //   ],
+      // ),
+      backgroundColor: const Color(0xffDDA15E),
+      nextScreen: const HomeScreen(),
+      splashIconSize: 350,
+      duration: 2100,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.fade,
     );
   }
 }
